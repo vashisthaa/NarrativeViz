657330d67e4bb5b69eb29d56021003fb92c713dd
Vashistha <vashistha.singh@gmail.com>
2021-07-26 01:47:16 -0700

    Added tooltip and cleaned up the interface

--- scripts/chart_viz.js
+++ scripts/chart_viz.js
@@ -1,3 +1,10 @@
+const vizobject = {
+    color_category: d3.schemeCategory20,
+    CountryColorMap: new Map(),
+    graph: null,
+    countryMap: new Map()
+}
+
 const rowConverter = function (d) {
     return {
         //Make a new Date object for each year + month
@@ -8,25 +15,19 @@ const rowConverter = function (d) {
         Fertility: parseFloat(d.Adolescent_Fertility)
     };
 }
-const vizobject = {
-    color_category: d3.schemeCategory20,
-    CountryColorMap: new Map(),
-    graph: null,
-    countryMap: new Map()
-}
 
-function generateCheckBoxes(countries){
+function generateCheckBoxes(countries) {
     var countryTuples = [];
-    for(i in countries){
+    for (i in countries) {
         let code = vizobject.countryMap.get(countries[i])[0].Country_Code;
         let name = vizobject.countryMap.get(countries[i])[0].Country;
         let color = vizobject.CountryColorMap.get(code);
-        let tuple = [name,code,color];
+        let tuple = [name, code, color];
 
         countryTuples.push(tuple);
     }
     countryTuples.sort();
-    console.log(countryTuples);
+    //console.log(countryTuples);
     const input_type_checkbox_part1 = '<input type="checkbox" id="';
     const input_type_checkbox_part2 = '" name="';
     const input_type_checkbox_part3 = '" value=';
@@ -42,24 +43,24 @@ function generateCheckBoxes(countries){
         '  <rect width="12" height="12" style="fill:color_code_placeholder;stroke-width:3;stroke:color_code_placeholder"/>\n' +
         '</svg>';
     var finalResult = '';
-    for(i in countryTuples){
+    for (i in countryTuples) {
         let newcolorpalette = colorpalette.replaceAll('color_code_placeholder', countryTuples[i][2]);
         //let newcolorpalette = '';
         //console.log(countryTuples[i][0]);
-        let temp = newcolorpalette+ input_type_checkbox_part1
-                        +countryTuples[i][1]
-                        +input_type_checkbox_part2
-                        +countryTuples[i][1]
-                        +input_type_checkbox_part3
-                        +countryTuples[i][0]
-                        +input_type_checkbox_part4
-                        +closing_bracket
-                        +input_type_label_part1
-                        +countryTuples[i][1]
-                        +input_type_label_part2
-                        +countryTuples[i][0]
-                        +input_type_label_part3
-       //console.log(vizobject.countryMap.get(countries[i])[0]);
+        let temp = newcolorpalette + input_type_checkbox_part1
+            + countryTuples[i][1]
+            + input_type_checkbox_part2
+            + countryTuples[i][1]
+            + input_type_checkbox_part3
+            + countryTuples[i][0]
+            + input_type_checkbox_part4
+            + closing_bracket
+            + input_type_label_part1
+            + countryTuples[i][1]
+            + input_type_label_part2
+            + countryTuples[i][0]
+            + input_type_label_part3
+        //console.log(vizobject.countryMap.get(countries[i])[0]);
         //console.log(temp);
         finalResult = finalResult + temp;
 
@@ -68,7 +69,19 @@ function generateCheckBoxes(countries){
     let container = document.getElementById('filter_container');
     container.innerHTML = finalResult;
 }
-function onbodyload(scene) {
+
+function onbodyload() {
+    const params = new URLSearchParams(window.location.search);
+    const scene_param = params.get('scene');
+    if(parseInt(scene_param) == 3){
+        //console.log(document.getElementById('filter_country').style.padding);
+        //document.getElementById('filter_country').remove();
+        document.getElementById("filter_country").style.display = "inline";
+    }
+    let scene_name = ('Scene ') + (scene_param==null ? '1' : scene_param);
+    //console.log(scene_name);
+    document.getElementById('top_level_box_id').textContent = scene_name;
+
     let dataset = d3.csv("./data/WDI_Adolescent_Fertility_cleaned.csv", rowConverter, function (data) {
         const countries = d3.map(data, function (d) {
             return (d.Country_Code)
@@ -88,28 +101,30 @@ function onbodyload(scene) {
             }
 
         }
-        generateCheckBoxes(countries);
+
+        if(parseInt(scene_param) == 3){
+            generateCheckBoxes(countries);
+        }
+
 
         let graph = {};
-        //graph.xAxis_Length = d3.max(data, function(d) { return d.Year; }) + 10;
-        //graph.xAxis_Origin = d3.min(data, function(d) { return d.Year; });
-        //graph.yAxis_Length = d3.max(data, function(d) { return d.Fertility; }) + 10;
-        //graph.yAxis_Origin = d3.min(data, function(d) { return d.Fertility; });
         graph.xAxis_Length = 2020;
         graph.xAxis_Origin = 1960;
         graph.yAxis_Length = 240;
         graph.yAxis_Origin = 0;
         vizobject.graph = graph;
-        //drawGraphV2();
         //console.log(graph.xAxis_Origin);
         //console.log(graph.xAxis_Origin);
         //console.log(graph.yAxis_Length);
         //console.log(graph.yAxis_Origin);
         drawGraph();
-
-        //drawLineChartForCountry('LMC');
-        //drawLineChartForCountry('HIC');
-        drawLineChartForCountry('GAB');
+        if(scene_param==null || parseInt(scene_param) == 1){
+            drawLineChartForCountry('WLD');
+        }
+        if(parseInt(scene_param) == 2){
+            drawLineChartForCountry('WLD');
+            drawLineChartForCountry('LIC');
+        }
         //removeLineChartForCountry('ZWE');
 
         //alert('hello');
@@ -119,13 +134,12 @@ function onbodyload(scene) {
 }
 
 
-
 function drawGraph() {
-    var margin = {top: 10, right: 10, bottom: 145, left: 50};
-    var width = 1100 - margin.left - margin.right;
-    var height = 650 - margin.top - margin.bottom;
-    var svg = d3.select("svg");
+    var margin = 200;
 
+    var svg = d3.select("svg");
+    width = svg.attr("width") - margin, //300
+    height = svg.attr("height") - margin //200
 
 
     vizobject.graph.xScale = d3.scaleLinear().domain([vizobject.graph.xAxis_Origin, vizobject.graph.xAxis_Length]).range([0, width]);
@@ -134,8 +148,7 @@ function drawGraph() {
     var g = svg.append("g")
         .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-    // Step 5
-    // Title
+
     svg.append('text')
         .attr('x', width / 2 + 100)
         .attr('y', 100)
@@ -144,7 +157,7 @@ function drawGraph() {
         .style('font-size', 20)
         .text('Adolescent Fertility Rate Trends');
 
-    // X label
+
     svg.append('text')
         .attr('x', width / 2 + 100)
         .attr('y', height - 15 + 170)
@@ -153,7 +166,7 @@ function drawGraph() {
         .style('font-size', 14)
         .text('Year [1960 - 2019]');
 
-    // Y label
+
     svg.append('text')
         .attr('text-anchor', 'right')
         .attr('transform', 'translate(60,' + height + ')rotate(-90)')
@@ -161,25 +174,27 @@ function drawGraph() {
         .style('font-size', 14)
         .text('Adolescent Fertility Rate');
 
-    // Step 6
+
     g.append("g")
-        .attr("transform", "translate(0,"+height+")")
+        .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(vizobject.graph.xScale));
 
     g.append("g")
         .call(d3.axisLeft(vizobject.graph.yScale));
 
 }
-function handleCheckBoxEvent(country){
+
+function handleCheckBoxEvent(country) {
     //alert(countrycode);
     let checkbox = document.getElementById(country.id);
-    if(checkbox.checked){
+    if (checkbox.checked) {
         drawLineChartForCountry(country.id);
-    }else{
+    } else {
         removeLineChartForCountry(country.id);
     }
     //console.log(country.id);
 }
+
 function drawLineChartForCountry(countryid) {
     let svg = d3.select("svg");
     let countrydata = Object.values(vizobject.countryMap.get(countryid));
@@ -194,6 +209,7 @@ function drawLineChartForCountry(countryid) {
     group.append("path")
         .datum(countrydata)
         .attr("class", "line")
+        .attr("transform", "translate(" + 100 + "," + 100 + ")")
         .style("fill", "none")
         .style("stroke", color)
         .style("stroke-width", "2")
@@ -203,19 +219,27 @@ function drawLineChartForCountry(countryid) {
                     return vizobject.graph.xScale(d[0]);
                 })
                 .y(function (d) {
-                    console.log(d);
+                    //console.log(d);
                     return vizobject.graph.yScale(d[3]);
-                })
-                (d)
+                })(d)
         })
     var finalx;
     var finaly;
     var label;
+
+
+    // Define the div for the tooltip
+    var tooltipdiv = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
     group
         .selectAll("dot")
         .data(countrydata)
         .enter()
         .append("circle")
+        .attr("transform", "translate(" + 100 + "," + 100 + ")")
         .attr("cx", function (d) {
             finalx = d[0];
             return vizobject.graph.xScale(d[0]);
@@ -225,18 +249,36 @@ function drawLineChartForCountry(countryid) {
             label = d[1];
             return vizobject.graph.yScale(d[3]);
         })
-        .attr("r", 2.5)
+        .attr("r", 4)
         .attr("stroke", 'white')
+        .on("mouseover", function(d) {
+            //console.log(d3.event.pageX);
+            //console.log(d3.event.pageY);
+            tooltipdiv.transition()
+                .duration(200)
+                .style("opacity", 1);
+            tooltipdiv.html( "Country: <b>"+ d[1] +"</b><br/>Year:<b> " + d[0] + " </b><br/>Fertility:<b> "  + d[3] + "</b>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 70) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltipdiv.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
         .style("fill", color)
-    console.log(finalx);
-    console.log(finaly);
-    console.log(label);
-
-    group.selectAll("text")
-        .append("text")
-        .attr("x", 1200)
-        .attr("y", 400)
-        .attr("fill", 'black')
+    //console.log(finalx);
+    //console.log(finaly);
+    //console.log(label);
+    //if(label.length > 10){
+      //  label = label.substring(0,9) + '...'
+    //}
+    group.append("text")
+        .attr("transform", "translate("  + vizobject.graph.xScale(finalx + 7) + "," + vizobject.graph.yScale(finaly-53) + ")")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "start")
+        .style("fill", 'black')
+        .style("font-size", '10px')
         .text(label);
 }
 
@@ -248,20 +290,16 @@ function removeLineChartForCountry(countryid) {
 
 
 function onclickScene1() {
-    let countries = d3.map(vizobject.dataset, function (d) {
-        return (d.Country_Code)
-    }).keys();
-    alert(countries.size);
-    window.location.replace("./scene1.html");
+    window.location.replace("./story.html?scene=1");
 }
 
 function onclickScene2() {
-    window.location.replace("./scene2.html");
+    window.location.replace("./story.html?scene=2");
 
 }
 
 function onclickScene3() {
-    window.location.replace("./scene3.html");
+    window.location.replace("./story.html?scene=3");
 }
 
 
